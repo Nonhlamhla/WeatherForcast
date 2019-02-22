551a1a96e54d2c61fba52f0c2ae2ae78830f49f6
nonhlanhla <mkhonza.s@live.com>
2019-02-22 10:10:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/nonhlanhla/weatherforcast/tasks/ParseResult.java
@@ -0,0 +1,3 @@
+package com.example.nonhlanhla.weatherforcast.tasks;
+
+public enum ParseResult {OK, JSON_EXCEPTION, CITY_NOT_FOUND}
---
+++ app/src/main/java/com/example/nonhlanhla/weatherforcast/tasks/TaskResult.java
@@ -0,0 +1,3 @@
+package com.example.nonhlanhla.weatherforcast.tasks;
+
+public enum TaskResult { SUCCESS, BAD_RESPONSE, IO_EXCEPTION, TOO_MANY_REQUESTS}
